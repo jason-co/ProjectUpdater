abd3827d33bc34b017ee0f72496f442de7bb3ef1
InsideJC <InsideJC@gmail.com>
2015-11-02 10:21:00 -0800

    Including missing files

---
+++ ProjectUpdater/UpdaterLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Core;
+
+namespace ProjectUpdater
+{
+    public class UpdaterLogger : ViewModelBase, ILogger
+    {
+        private readonly ObservableCollection<string> _logs;
+
+        public UpdaterLogger()
+        {
+            _logs = new ObservableCollection<string>();
+        }
+        public IReadOnlyList<string> Logs { get { return new ReadOnlyObservableCollection<string>(_logs); } }
+
+        public void Log(string format, params object[] args)
+        {
+            BeginUpdateUI(() => _logs.Add(String.Format(format, args)));
+        }
+    }
+}
---
+++ ProjectUpdater/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+
+namespace ProjectUpdater.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly ILogger _logger;
+
+        public MainViewModel(ILogger logger)
+        {
+            _logger = logger;
+        }
+    }
+}
